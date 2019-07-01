d39142315b6d6fe6b31b4455ceabeb908c94f148
a <awatkin.work@gmail.com>
2019-07-01 21:45:43 +0100

    init and installing third party modules and using undescore

---
+++ npm-demo/index.js
@@ -0,0 +1,9 @@
+var _ = require("underscore");
+
+// Steps require takes to import something
+// Core module - in the system path for Node
+// File or folder - in the current directory
+// insude the node_module folder - in the current directory
+
+var result = _.contains([1, 2, 3], 2);
+console.log(result);
