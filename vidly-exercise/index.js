79e38fa497b54ce51043a8b02571f393550d1559
a <awatkin.work@gmail.com>
2019-07-02 23:05:53 +0100

    fucked up the name, changed categories to genres

--- vidly-exercise/index.js
+++ vidly-exercise/index.js
@@ -9,14 +9,14 @@ const app = express();
 app.use(express.json());
 
 // dummy data
-const videoCategories = [
+const genres = [
   { id: "1", name: "action" },
   { id: "2", name: "comdey" },
   { id: "3", name: "romance" },
   { id: "4", name: "animation" }
 ];
 
-function validateCategory(cat) {
+function validateGenre(cat) {
   // validation object
   const schema = {
     id: Joi.string().required(),
@@ -28,12 +28,12 @@ function validateCategory(cat) {
   return Joi.validate(cat, schema);
 }
 
-app.get("/api/categories", (req, res) => {
-  return res.send(videoCategories);
+app.get("/api/genres", (req, res) => {
+  return res.send(genres);
 });
 
-app.get("/api/categories/:id", (req, res) => {
-  const cat = videoCategories.find(c => c.id === req.params.id);
+app.get("/api/genres/:id", (req, res) => {
+  const cat = genres.find(c => c.id === req.params.id);
 
   if (!cat) {
     return res.status(404).send("Only dogs here.");
@@ -42,53 +42,53 @@ app.get("/api/categories/:id", (req, res) => {
   return res.send(cat);
 });
 
-app.post("/api/categories", (req, res) => {
-  const cat = videoCategories.find(c => c.id === req.params.id);
+app.post("/api/genres", (req, res) => {
+  const cat = genres.find(c => c.id === req.params.id);
 
   if (cat) {
     return res.status(409).send("Already present.");
   }
 
-  const { error } = validateCategory(req.body);
+  const { error } = validateGenre(req.body);
   // for fuck sake this took too long
   if (error) {
     return res.status(409).send(error.details[0].message);
   }
 
-  videoCategories.push(req.body);
-  console.log(videoCategories);
+  genres.push(req.body);
+  console.log(genres);
   // 201 resource creates
   return res.status(201).send(req.body);
 });
 
-app.put("/api/categories/:id", (req, res) => {
+app.put("/api/genres/:id", (req, res) => {
   // check resource exists
-  const cat = videoCategories.find(c => c.id === req.params.id);
+  const cat = genres.find(c => c.id === req.params.id);
 
   if (!cat) {
     return res.status(404).send("Resource does not exist.");
   }
 
   // validate input
-  const { error } = validateCategory(req.body);
+  const { error } = validateGenre(req.body);
 
   if (error) {
     return res.status(400).send(error.details[0].message);
   }
 
   // find the index in the collection
-  const index = videoCategories.indexOf(cat);
+  const index = genres.indexOf(cat);
   // replace current object - remember you have to tell it how many to replace
-  videoCategories.splice(index, 1, req.body);
-  console.log(videoCategories);
+  genres.splice(index, 1, req.body);
+  console.log(genres);
 
   // send new object back to client
   return res.status(200).send(req.body);
 });
 
-app.delete("/api/categories/:id", (req, res) => {
+app.delete("/api/genres/:id", (req, res) => {
   let catIndex = null;
-  const cat = videoCategories.find((c, index) => {
+  const cat = genres.find((c, index) => {
     if (c.id === req.params.id) {
       catIndex = index;
       return c.id === req.params.id;
@@ -99,7 +99,7 @@ app.delete("/api/categories/:id", (req, res) => {
     return res.status(404).send("Resource does not exist.");
   }
 
-  const deletedCat = videoCategories.splice(catIndex, 1);
+  const deletedCat = genres.splice(catIndex, 1);
   res.status(200).send(deletedCat);
 });
 
