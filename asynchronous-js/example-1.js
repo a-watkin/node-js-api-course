c507bc7d8078ed34f0f08121b4fff38b28413b63
a <awatkin.work@gmail.com>
2019-07-04 16:58:18 +0100

    notes and callback exercise attempt

--- asynchronous-js/example-1.js
+++ asynchronous-js/example-1.js
@@ -1,10 +1,20 @@
 console.log("Before");
+const user = getUser(1, function(user) {
+  console.log("user", user);
+
+  // Get repos
+  getRepositories(user.gitHubUsername, function(data) {
+    console.log(data["repositories"]);
+  });
+});
 
 // asynchronous and non-blocking
 // schedules a task to performe in the future
-setTimeout(() => {
-  console.log("Retreiving a user from a database...");
-}, 2000);
+// it's still a single thread - there's just a queue for the thread
+// it is NOT concurrent
+// setTimeout(() => {
+//   console.log("Retreiving a user from a database...");
+// }, 2000);
 
 console.log("After");
 
@@ -12,3 +22,23 @@ console.log("After");
 // before
 // after
 // Retreiving a user from a database...
+
+// callback is a function that's called after an asynch function has finished
+function getUser(id, callback) {
+  setTimeout(() => {
+    console.log("Retreiving a user from a database...");
+    callback({
+      id: id,
+      gitHubUsername: "a"
+    });
+  }, 2000);
+}
+
+function getRepositories(username, callback) {
+  setTimeout(() => {
+    callback({
+      username: username,
+      repositories: ["repo1", "repo2", "repo3"]
+    });
+  }, 2000);
+}
