31f50c8c4a900ebcfc91c12196a7b64a21a503cb
a-watkin <awatkin.work@gmail.com>
2019-07-06 21:39:32 +0100

    getting all documents

--- mongo-db/index.js
+++ mongo-db/index.js
@@ -5,35 +5,44 @@ mongoose
   .then(() => console.log("Connected to MongoDB..."))
   .catch(err => console.log("Could not connect to mongodb..."));
 
-const courseSchema = new mongoose.Schema(
-    name: String,
-    author: String,
-    tags: [String],
-    data, {type: Date, default: Date.now},
-    isPublished: Boolean
-);
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  data: { type: Date, default: Date.now },
+  isPublished: Boolean
+});
 
 // compile schema into a model
 // name of the collection and the schema that defines it
 // you don't need to have the date here because it's defined above as having a default value
-mongoose.model('Courses', courseSchema)
+const Course = mongoose.model("Course", courseSchema);
 
 // inside an async function because it's and asynchronous operation
 async function createCourse() {
-
+  try {
     const course = new Course({
-        name: 'Node.js Course',
-        author: 'A',
-        tags: ['node', 'backend'],
-        isPublished: true
-    })
+      name: "Node.js Course",
+      author: "A",
+      tags: ["React", "frontend"],
+      isPublished: true
+    });
 
     // asynchronous operation
     // when it's done result will contain the databse object
-    const result = await course.save()
-    console.log(result)
-
+    const result = await course.save();
+    console.log(result);
+  } catch (error) {
+    console.log("Something went wrong, ", error);
+  }
 }
 
+// createCourse();
 
-createCourse()
+// get all documents
+async function getCourses() {
+  const courses = await Course.find();
+  console.log(courses);
+}
+
+getCourses();
