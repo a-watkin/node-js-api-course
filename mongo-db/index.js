ee9c9c4c1c1a4e7c2804d78d6ceec68b22f472d2
a-watkin <awatkin.work@gmail.com>
2019-07-06 22:33:19 +0100

    limiting query results

--- mongo-db/index.js
+++ mongo-db/index.js
@@ -41,7 +41,12 @@ async function createCourse() {
 
 // get all documents
 async function getCourses() {
-  const courses = await Course.find();
+  const courses = await Course.find({ author: "A", isPublished: true })
+    .limit(1)
+    // 1 indicates ascending order, -1 descending
+    .sort({ name: 1 })
+    // what you want to select and the number of things to select
+    .select({ tags: 1, name: 1 });
   console.log(courses);
 }
 
