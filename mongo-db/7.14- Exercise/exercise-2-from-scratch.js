9134f6e29c4fcb903f397f08388525d4bc430181
a-watkin <awatkin.work@gmail.com>
2019-07-08 21:12:43 +0300

    finished second exercise for 7.14

--- mongo-db/7.14- Exercise/exercise-2-from-scratch.js
+++ mongo-db/7.14- Exercise/exercise-2-from-scratch.js
@@ -3,6 +3,32 @@ const mongoose = require("mongoose");
 mongoose
   .connect("mongodb://localhost/mongo-exercises", { useNewUrlParser: true })
   .then(() => console.log("connected"))
-  .catch(() => console.log("not connected"));
+  .catch(err => console.log("not connected", err));
 
-mongoose.disconnect();
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  price: Number,
+  data: { type: Date, default: Date.now },
+  isPublished: Boolean
+});
+
+const Course = mongoose.model("Course", courseSchema);
+
+async function publishedCourses() {
+  try {
+    const courses = await Course.find({ isPublished: true })
+      .sort("-price")
+      .select(["name", "author"]);
+    console.log(courses);
+  } catch (error) {
+    console.log("this some bullshit");
+  } finally {
+    // moving this to the bottom of the file causes problems
+    // probably due to it being async
+    mongoose.disconnect();
+  }
+}
+
+publishedCourses();
