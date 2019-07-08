d4d0cba481c91febd613401191f81b8473e0a03a
a-watkin <awatkin.work@gmail.com>
2019-07-09 01:21:33 +0300

    note about logical or and how to us it with mongo

--- mongo-db/7.14- Exercise/exercise-2-from-scratch.js
+++ mongo-db/7.14- Exercise/exercise-2-from-scratch.js
@@ -19,12 +19,14 @@ const Course = mongoose.model("Course", courseSchema);
 async function getPublishedFrontBackend() {
   try {
     const courses = await Course.find({
-      isPublished: true,
-      tags: { $in: ["frontend", "backend"] }
+      isPublished: true
+      //   tags: { $in: ["frontend", "backend"] }
     })
+      // don't put the tag name in an []
+      .or([{ tags: "frontend" }, { tags: "backend" }])
       .sort("-price")
       // can also select the attributes like so:
-      .select("name author price");
+      .select("name isPublished author tags price");
     console.log(courses);
   } catch (error) {
     console.log(error);
