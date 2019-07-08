a75365126e235a33f171e225e8f5e1f3d7bb539c
a-watkin <awatkin.work@gmail.com>
2019-07-08 10:59:51 +0300

    getting count example

---
+++ mongo-db/counting-documents.js
@@ -0,0 +1,19 @@
+const { Course, mongoose } = require("./db-interface");
+// or
+// and
+
+async function getCourses() {
+  try {
+    const courses = await Course.find({
+      author: /^A/
+    }).count();
+    console.log(courses);
+  } catch (error) {
+    console.log("Problem ", error);
+  } finally {
+    // Disconnect from the database
+    mongoose.disconnect();
+  }
+}
+
+getCourses();
