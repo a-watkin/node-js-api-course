ef4bc00d35b1a2c134424caa63df23e7459414cc
a-watkin <awatkin.work@gmail.com>
2019-07-08 10:53:54 +0300

    notes on logical operators and code examples

--- mongo-db/logical-query-operators.js
+++ mongo-db/logical-query-operators.js
@@ -1,8 +1,18 @@
 const { Course, mongoose } = require("./db-interface");
+// or
+// and
 
 async function getCourses() {
   try {
-    const courses = await Course.find({ author: "A" } && { isPublished: true });
+    const courses = await Course
+      // how i did it
+      // .find(
+      //   { author: "A" } && { isPublished: false }
+      // );
+      // how it's actually done
+      .find()
+      .or([{ price: 15 }, { isPublished: false }])
+      .and([{ author: "Adam" }]);
     console.log(courses);
   } catch (error) {
     console.log("Problem ", error);
