ce063dfd0faf97c796182b7e3b8db0475d4fc96a
a-watkin <awatkin.work@gmail.com>
2019-07-09 01:09:01 +0300

    finished my solution

--- mongo-db/7.14- Exercise/exercise-2-from-scratch.js
+++ mongo-db/7.14- Exercise/exercise-2-from-scratch.js
@@ -3,32 +3,33 @@ const mongoose = require("mongoose");
 mongoose
   .connect("mongodb://localhost/mongo-exercises", { useNewUrlParser: true })
   .then(() => console.log("connected"))
-  .catch(err => console.log("not connected", err));
+  .catch(err => console.log("Problems ", err));
 
-const courseSchema = new mongoose.Schema({
+const courseSchema = {
   name: String,
   author: String,
-  tags: [String],
   price: Number,
-  data: { type: Date, default: Date.now },
-  isPublished: Boolean
-});
+  isPublished: Boolean,
+  date: { type: Date, default: Date.now },
+  tags: [String]
+};
 
 const Course = mongoose.model("Course", courseSchema);
 
-async function publishedCourses() {
+async function getPublishedFrontBackend() {
   try {
-    const courses = await Course.find({ isPublished: true })
+    const courses = await Course.find({
+      isPublished: true,
+      tags: { $in: ["frontend", "backend"] }
+    })
       .sort("-price")
       .select(["name", "author"]);
     console.log(courses);
   } catch (error) {
-    console.log("this some bullshit");
+    console.log(error);
   } finally {
-    // moving this to the bottom of the file causes problems
-    // probably due to it being async
     mongoose.disconnect();
   }
 }
 
-publishedCourses();
+getPublishedFrontBackend();
