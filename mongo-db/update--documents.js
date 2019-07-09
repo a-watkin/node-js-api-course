474c0c9679188b5fc2c60048745a1c7e6e65ec7d
a-watkin <awatkin.work@gmail.com>
2019-07-09 18:29:40 +0300

    updating a course by first finding it

---
+++ mongo-db/update--documents.js
@@ -0,0 +1,32 @@
+const { Course, mongoose } = require("./db-interface");
+
+// update a document where you know the id
+async function updateCourse(id) {
+  try {
+    const course = await Course.findById(id);
+    if (!course) {
+      return;
+    }
+    // update details
+    course.isPublished = true;
+    course.author = "Another Author";
+    // identical to above
+    course.set({
+      isPublished: true,
+      author: "Another Author"
+    });
+
+    try {
+      const result = await course.save();
+      console.log(result);
+    } catch (error) {
+      console.log("error updating...", result);
+    }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    mongoose.disconnect();
+  }
+}
+
+updateCourse("5d2229bebfd6184c0fd60635");
