77fb92fd4188bfbb439c41eaf25c3665404086d2
a-watkin <awatkin.work@gmail.com>
2019-07-10 23:04:15 +0300

    added debugging messages, conifg and helmet

--- vidly-exercise/index.js
+++ vidly-exercise/index.js
@@ -5,21 +5,35 @@ const morgan = require("morgan");
 // So you DON'T captitalise express
 const router = require("./routes/genres");
 
+// loads configs
+const config = require("config");
 // you can have multiple debuggers
 const startupDebugger = require("debug")("app:startup");
-
+// Helmet helps you secure your Express apps by setting various HTTP headers
+const helmet = require("helmet");
 // express instance
 const app = express();
-// logging module - it logs http requests, tiny being the level of logging
-app.use(morgan("tiny"));
-startupDebugger("Morgan enabled...");
+
+// two ways to get env variables
+console.log(app.get("env"));
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+
+console.log("Application name: " + config.get("name"));
+
+// enabling logging only if env is development
+if (app.get("env") === "development") {
+  // logging module - it logs http requests, tiny being the level of logging
+  app.use(morgan("tiny"));
+  // debugger equivalent of above
+  startupDebugger("Morgan enabled...");
+}
+
+// tells express to ue helmet to secure http headers
+// should be applied early to be effective
+app.use(helmet());
 // tell express to use JSON - you MUST put this before an routes
 app.use(express.json());
 app.use("/api/genres", router);
-
-// Helmet helps you secure your Express apps by setting various HTTP headers
-const helmet = require("helmet");
-app.use(helmet());
 
 // either use an env variable or 3000
 // process global
