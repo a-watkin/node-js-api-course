3fed4b6cce52a0af4ce66e7374fd5dcf6109edf9
a-watkin <awatkin.work@gmail.com>
2019-07-10 17:14:42 +0300

    working on vidly to add mongo db

--- vidly-exercise/index.js
+++ vidly-exercise/index.js
@@ -1,12 +1,25 @@
 // Express framework
 const express = require("express");
+// logging
+const morgan = require("morgan");
 // So you DON'T captitalise express
 const router = require("./routes/genres");
+
+// you can have multiple debuggers
+const startupDebugger = require("debug")("app:startup");
+
 // express instance
 const app = express();
+// logging module - it logs http requests, tiny being the level of logging
+app.use(morgan("tiny"));
+startupDebugger("Morgan enabled...");
 // tell express to use JSON - you MUST put this before an routes
 app.use(express.json());
 app.use("/api/genres", router);
+
+// Helmet helps you secure your Express apps by setting various HTTP headers
+const helmet = require("helmet");
+app.use(helmet());
 
 // either use an env variable or 3000
 // process global
@@ -14,5 +27,5 @@ const port = process.env.PORT || 3000;
 
 // start the app listening
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+  startupDebugger(`Listening on port ${port}`);
 });
