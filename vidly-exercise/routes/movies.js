ec8f01fad040ae0b9ec516db6c149e5ef007dbf1
a-watkin <awatkin.work@gmail.com>
2019-07-21 23:12:35 +0300

    moview update troublshooting

--- vidly-exercise/routes/movies.js
+++ vidly-exercise/routes/movies.js
@@ -64,6 +64,43 @@ router.post("/", async (req, res) => {
 // put
 // delete
 
+router.put("/:id", async (req, res) => {
+  try {
+    const currMovie = await Movie.findOne({ _id: req.params.id });
+    if (!currMovie) {
+      return res.status(400).send("movie not found");
+    }
+    info(req.params.id);
+    const combined = { ...currMovie._doc, ...req.body };
+
+    try {
+      info("combined is ", combined);
+      // The default is to return the original, unaltered document. If you want the new, updated document to be returned you have to pass an additional argument: an object with the new property set to true.
+      // the above still didn't work for me
+
+      // this seems to work better
+
+      // {new: true} does not work even a little
+      const update = await Movie.findOneAndUpdate(
+        combined
+        // i also tried passing args in {} before below and it still didn't work
+        // { new: true }
+      );
+      info("update here ", update);
+      if (!update) {
+        info("update not populated", update);
+      }
+
+      info("update is ", update);
+      return res.send(update);
+    } catch (error) {
+      info("problem saving update ", error);
+    }
+  } catch (error) {
+    return res.send(error);
+  }
+});
+
 router.delete("/:id", async (req, res) => {
   try {
     const deleteMovie = await Movie.findOneAndRemove({ _id: req.params.id });
