eaba07f31bdf11a7db44952e28742d5a8744d087
a-watkin <awatkin.work@gmail.com>
2019-07-22 23:27:44 +0300

    changed to authors version

--- vidly-exercise/routes/movies.js
+++ vidly-exercise/routes/movies.js
@@ -1,116 +1,78 @@
-const info = require("debug")("app:info");
-// Input validation
+const { Movie, validate } = require("../models/movie");
+const { Genre } = require("../models/genre");
+const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
-const { Movie, validateMovie, addMovie } = require("../models/movie");
-const { Genre } = require("../models/genre");
-
-router.get("/create", (req, res) => {
-  try {
-    addMovie("the shape of water", new Genre({ name: "fantasy" }));
-  } catch (error) {
-    info("error adding movie ", error);
-  }
-  res.send("test");
-});
 
 router.get("/", async (req, res) => {
-  // query to get all movies
-  Movie.find()
-    .then(result => {
-      res.send(result);
-    })
-    .catch(err => info("problem getting movies: ", err));
+  const movies = await Movie.find().sort("name");
+  res.send(movies);
 });
-
-router.get("/:id", async (req, res) => {
-  Movie.findOne({ _id: req.params.id })
-    .then(result => {
-      res.send(result);
-    })
-    .catch(err => info("not finding that movie"));
-});
-
-function mapToGenres(genres) {
-  return genres.map((elem, ind, arr) => {
-    return (arr[ind] = new Genre({ name: elem }));
-  });
-}
 
 router.post("/", async (req, res) => {
-  try {
-    const genres = mapToGenres(req.body.genres);
-    const title = req.body.title;
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    const newMovie = new Movie({
-      title,
-      genres
-    });
+  const genre = await Genre.findById(req.body.genreId);
+  if (!genre) return res.status(400).send("Invalid genre.");
 
-    const saveResult = await newMovie
-      .save()
-      .then(result => res.send(result))
-      .catch(err => res.send("problem saving", err));
+  let movie = new Movie({
+    title: req.body.title,
+    genre: {
+      _id: genre._id,
+      name: genre.name
+    },
+    numberInStock: req.body.numberInStock,
+    dailyRentalRate: req.body.dailyRentalRate
+  });
+  movie = await movie.save();
 
-    // res.send(movie);
-  } catch (error) {
-    info(error);
-  }
+  res.send(movie);
 });
 
-// get
-// post
-// put
-// delete
-
 router.put("/:id", async (req, res) => {
-  // returns the document as it was before update currently
-  try {
-    const currMovie = await Movie.findOne({ _id: req.params.id });
-    if (!currMovie) {
-      return res.status(400).send("movie not found");
-    }
-    info(req.params.id);
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    // make any new genres into Genre objects
-    req.body.genres = mapToGenres(req.body.genres);
-    const combined = { ...currMovie._doc, ...req.body };
+  const genre = await Genre.findById(req.body.genreId);
+  if (!genre) return res.status(400).send("Invalid genre.");
 
-    try {
-      info("combined is ", combined);
-      // The default is to return the original, unaltered document. If you want the new, updated document to be returned you have to pass an additional argument: an object with the new property set to true.
-      // the above still didn't work for me
+  const movie = await Movie.findByIdAndUpdate(
+    req.params.id,
+    {
+      title: req.body.title,
+      genre: {
+        _id: genre._id,
+        name: genre.name
+      },
+      numberInStock: req.body.numberInStock,
+      dailyRentalRate: req.body.dailyRentalRate
+    },
+    { new: true }
+  );
 
-      // this seems to work better
+  if (!movie)
+    return res.status(404).send("The movie with the given ID was not found.");
 
-      // {new: true} does not work even a little
-      const update = await Movie.findOneAndUpdate(
-        combined
-        // i also tried passing args in {} before below and it still didn't work
-        // { new: true }
-      );
-      info("update here ", update);
-      if (!update) {
-        info("update not populated", update);
-      }
-
-      info("update is ", update);
-      return res.send(update);
-    } catch (error) {
-      info("problem saving update ", error);
-    }
-  } catch (error) {
-    return res.send(error);
-  }
+  res.send(movie);
 });
 
 router.delete("/:id", async (req, res) => {
-  try {
-    const deleteMovie = await Movie.findOneAndRemove({ _id: req.params.id });
-    res.send(deleteMovie);
-  } catch (error) {
-    res.send(error);
-  }
+  const movie = await Movie.findByIdAndRemove(req.params.id);
+
+  if (!movie)
+    return res.status(404).send("The movie with the given ID was not found.");
+
+  res.send(movie);
+});
+
+router.get("/:id", async (req, res) => {
+  const movie = await Movie.findById(req.params.id);
+
+  if (!movie)
+    return res.status(404).send("The movie with the given ID was not found.");
+
+  res.send(movie);
 });
 
 module.exports = router;
