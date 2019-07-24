7c47c7fc7d83a1031143faefa8abfaaa58725c38
a-watkin <awatkin.work@gmail.com>
2019-07-24 04:19:04 +0300

    changed names

--- vidly-exercise/models/movie.js
+++ vidly-exercise/models/movie.js
@@ -4,34 +4,33 @@ const mongoose = require("mongoose");
 const Joi = require("@hapi/joi");
 const { genreSchema } = require("./genre");
 
-const Movie = mongoose.model(
-  "Movies",
-  new mongoose.Schema({
-    title: {
-      type: String,
-      required: true,
-      trim: true,
-      minlength: 5,
-      maxlength: 255
-    },
-    genre: {
-      type: genreSchema,
-      required: true
-    },
-    numberInStock: {
-      type: Number,
-      required: true,
-      min: 0,
-      max: 255
-    },
-    dailyRentalRate: {
-      type: Number,
-      required: true,
-      min: 0,
-      max: 255
-    }
-  })
-);
+const movieSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 5,
+    maxlength: 255
+  },
+  genre: {
+    type: genreSchema,
+    required: true
+  },
+  numberInStock: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 255
+  },
+  dailyRentalRate: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 255
+  }
+});
+
+const Movie = mongoose.model("Movie", movieSchema);
 
 function validateMovie(movie) {
   const schema = {
@@ -52,5 +51,4 @@ function validateMovie(movie) {
   return Joi.validate(movie, schema);
 }
 
-exports.Movie = Movie;
-exports.validate = validateMovie;
+module.exports = { Movie, validateMovie, movieSchema };
--- vidly-exercise/routes/movie.js
+++ vidly-exercise/routes/movie.js
@@ -1,4 +1,4 @@
-const { Movie, validate } = require("../models/movies");
+const { Movie, validate } = require("../models/movie");
 const { Genre } = require("../models/genre");
 const express = require("express");
 const router = express.Router();
