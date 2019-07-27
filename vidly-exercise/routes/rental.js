a7e62ff6f15ffb2040686605c0061c921955df36
a-watkin <awatkin.work@gmail.com>
2019-07-27 22:18:05 +0300

    experimenting with different method to get cusotmer record

--- vidly-exercise/routes/rental.js
+++ vidly-exercise/routes/rental.js
@@ -18,7 +18,7 @@ router.post("/", async (req, res) => {
     const { error } = validateRental(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    const customer = await Customer.findById(req.body.customerId);
+    const customer = await Customer.findOne(req.body.customerId);
     if (!customer) return res.status(400).send("Invalid customer.");
 
     const movie = await Movie.findById(req.body.movieId);
