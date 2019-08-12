23c6f405f85600f47dd043ed806ce078103f891a
a <awatkin.work@gmail.com>
2019-08-12 22:34:10 +0100

    put config in its own file from index.js

---
+++ vidly-exercise/startup/config.js
@@ -0,0 +1,20 @@
+const dotenv = require("dotenv");
+const result = dotenv.config();
+
+module.exports = function() {
+  if (result.error) {
+    console.log("Problem with env variables.");
+    throw result.error;
+  }
+
+  //   console.log(result.parsed);
+  if (!process.env.jwtPrivateKey) {
+    // this should be automatically caught by winston
+    // ALWAYS thorw an error object so you have access to the stack trace
+    throw new Error("FATAL ERROR: jwtPrivateKey is not defined");
+    // console.log("FATAL ERROR: jwtPrivateKey is not defined");
+    // global kill app - 1 indicated an error 0 is success
+    // process.exit(1);
+  }
+  //   console.log(process.env.jwtPrivateKey);
+};
