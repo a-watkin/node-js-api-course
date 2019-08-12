544ec195dca652a1e96d5a25bb8edfcc680b0d3d
a <awatkin.work@gmail.com>
2019-08-12 19:28:03 +0100

    refactoring to remove elements to startup

--- vidly-exercise/index.js
+++ vidly-exercise/index.js
@@ -1,3 +1,4 @@
+/* START - CONFIG */
 // dotenv
 const dotenv = require("dotenv");
 const result = dotenv.config();
@@ -7,16 +8,20 @@ if (result.error) {
   throw result.error;
 }
 
-console.log(result.parsed);
-
-/* CHANGE THIS */
+// console.log(result.parsed);
 if (!process.env.jwtPrivateKey) {
   console.log("FATAL ERROR: jwtPrivateKey is not defined");
   // global kill app - 1 indicated an error 0 is success
   process.exit(1);
 }
 console.log(process.env.jwtPrivateKey);
-/* ABOVE - CONFIG */
+/* END - CONFIG */
+
+// logging - to the console
+const morgan = require("morgan");
+
+// you can have multiple debuggers
+const startupDebugger = require("debug")("app:startup");
 
 // Express framework
 // So you DON'T capitalise express
@@ -25,87 +30,39 @@ const express = require("express");
 
 // monkey patches in async error handling - wraps end points in a try catch block in the same way as in asyncMiddleware
 // requiring it here is all that's necessary - it doesn't have to be stored as a constant
+//
+// is this middleware?
+//
 require("express-async-errors");
 
-// for logging
-// defualt is ok for small to medium apps only larget apps need to define a custom logger for different parts of the app
-const winston = require("winston");
-// write winston logs to mongodb
-require("winston-mongodb");
-
-// comes with a 'transport' for logging messages from the console
-winston.add(new winston.transports.File({ filename: "logfile.log" }));
-
-// this doens't work well
-winston.add(
-  new winston.transports.MongoDB({
-    db: "mongodb://localhost/vidly",
-    level: "silly"
-  })
-);
-
 // moved here from rental.js in models to make available to entire app
+//
+// does it?
+//
 const Joi = require("@hapi/joi");
 // passes Joi to the function retuned by joi-objectid
 Joi.objectId = require("joi-objectid")(Joi);
 
-// logging uncaughtExceptions with winston
-// i think this can only be used for unhandled exceptions?
-winston.exceptions.handle(
-  new winston.transports.File({ filename: "uncaughtExceptions.log" })
-);
-
-process.on("uncaughtException", ex => {
-  // this does the same as below - in this same body
-  throw ex;
-
-  // winston.error(ex.message, ex);
-  // process.exit(1);
-});
-
-// write process exception that happen with the app outside the context of express
-// such as mongodb going down
-// process.on("uncaughtException", ex => {
-//   console.log("AN EXCEPTION OCCURED");
-//   winston.error(ex.message, ex);
-//   process.exit(1);
-// });
-
-process.on("uncaughtException", ex => {
-  console.log("UNHANDLED EXCEPTION REJECTION");
-  winston.error(ex.message, ex);
-  process.exit(1);
-});
-
-// logging - to the console
-const morgan = require("morgan");
-
-const info = require("debug")("app:info");
-// info(process.env);
-
-// you can have multiple debuggers
-const startupDebugger = require("debug")("app:startup");
-// Helmet helps you secure your Express apps by setting various HTTP headers
-const helmet = require("helmet");
 // express instance
 const app = express();
+
+// calling logging elements
+require("./startup/logging")(app);
+
+// routes ruturns a function where app is passed as a variable
 require("./startup/routes")(app);
+// the () after require calls the function held in this file
+// it doesn't need an argument as it just connects to the db
+// this is a bit confusing
+require("./startup/db")();
 
 // two ways to get env variables
 console.log(app.get("env"));
 console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
 
-const mongoose = require("mongoose");
-// trying to surpress depreciation warning that has cropt up since adding a hashed password
-mongoose.set("useCreateIndex", true);
-
-const dbInfo = require("debug")("app:dbInfo");
-
-// it seems making the connection here makes it available throughout the app?
-mongoose
-  .connect("mongodb://localhost/vidly", { useNewUrlParser: true })
-  .then(() => dbInfo("Connected to MongoDB..."))
-  .catch(err => dbInfo("Could not connect to mongodb..."));
+// either use an env variable or 3000
+// process global
+const port = process.env.PORT || 5000;
 
 // enabling logging only if env is development
 if (app.get("env") === "development") {
@@ -115,11 +72,8 @@ if (app.get("env") === "development") {
   startupDebugger("Morgan enabled...");
 }
 
-// either use an env variable or 3000
-// process global
-const port = process.env.PORT || 5000;
-
+// const startupDebugger = require("./startup/logging");
 // start the app listening
 app.listen(port, () => {
-  startupDebugger(`Listening on port ${port}`);
+  // startupDebugger(`Listening on port ${port}`);
 });
