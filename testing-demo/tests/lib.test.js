7fd6af1a33044b8a9d1b1d85210cb34a66e47251
a <awatkin.work@gmail.com>
2019-08-13 04:07:14 +0100

    wrote tests and notes for greet and getCurrencies

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -20,3 +20,28 @@ describe("absolute", () => {
     expect(result).toBe(0);
   });
 });
+
+describe("greet", () => {
+  it("Should return Welcome along with the given string.", () => {
+    const result = lib.greet("Maya");
+    // expect(result).toBe("Welcome Maya");
+    // expect(result).toMatch(/Maya/);
+    expect(result).toContain("Maya");
+  });
+});
+
+describe("getCurrencies", () => {
+  it("should return an array of currencies.", () => {
+    const result = lib.getCurrencies();
+
+    // tests on arrays should not be to general - that it's defined
+    // or too sepcific - that elents at a place have a given value
+    // instead tests should just check if some meaningful value is in there somewhere
+    // expect(result).toContain("USD");
+    // expect(result).toContain("AUD");
+    // expect(result).toContain("EUR");
+
+    // checks these are in the array - they can be in any order
+    expect(result).toEqual(expect.arrayContaining(["EUR", "USD", "AUD"]));
+  });
+});
