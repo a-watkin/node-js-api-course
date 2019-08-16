62f1bf59ba241586a397854f2e078feb354c8a76
a <awatkin.work@gmail.com>
2019-08-16 21:36:33 +0100

    test passing after some debugging

--- vidly-exercise/tests/user.test.js
+++ vidly-exercise/tests/user.test.js
@@ -6,14 +6,19 @@ const mongoose = require("mongoose");
 // console.log(userSchema.methods.generateAuthToken());
 
 describe("generateAuthToken", () => {
-  it("should return a token", () => {
+  it("should return a valid JWT", () => {
     // console.log(config.get("jwtPrivateKey"));
-    const payload = { _id: new mongoose.Types.ObjectId(), isAdmin: true };
+    const payload = {
+      _id: new mongoose.Types.ObjectId().toHexString(),
+      isAdmin: true
+    };
     const user = new User(payload);
     const token = user.generateAuthToken();
-    const decoded = jwt.verify(token, "fuckyou");
-
+    // const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
+    const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
+    console.log(payload);
     console.log(decoded);
-    expect(decoded.toContain(payload));
+    console.log(config.get("jwtPrivateKey"));
+    expect(decoded).toMatchObject(payload);
   });
 });
