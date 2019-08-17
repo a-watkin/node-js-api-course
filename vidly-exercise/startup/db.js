a25b2faea2bf3074bdc0c43cdf44d10a4fec149a
a <awatkin.work@gmail.com>
2019-08-17 16:22:37 +0100

    getting db uri from config

--- vidly-exercise/startup/db.js
+++ vidly-exercise/startup/db.js
@@ -1,3 +1,4 @@
+const config = require("config");
 const winston = require("winston");
 const mongoose = require("mongoose");
 // trying to surpress depreciation warning that has cropt up since adding a hashed password
@@ -6,11 +7,12 @@ mongoose.set("useCreateIndex", true);
 // this didn't work until i passed it the app instance
 // i guess it uses it implicitly somehow?
 module.exports = function() {
+  const db = config.get("db");
   // it seems making the connection here makes it available throughout the app?
   mongoose
-    .connect("mongodb://localhost/vidly", { useNewUrlParser: true })
-    // now using winston instad of logging to console
-    .then(() => winston.info("Connected to MongoDB..."));
+    .connect(db, { useNewUrlParser: true })
+    // now using winston instead of logging to console
+    .then(() => winston.info(`Connected to ${db}`));
   // catch no longer needed? all it was going was logging to console before
   // i guess winston in index.js cataches exceptions? yeah it's global in scope to this app
   // .catch(err => console.log("Could not connect to mongodb...", err));
