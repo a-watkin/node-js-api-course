85f2ef0f5e89e168812f4701cf261b793bfae68b
a <awatkin.work@gmail.com>
2019-08-17 22:41:11 +0100

    validating objectId

--- vidly-exercise/routes/genre.js
+++ vidly-exercise/routes/genre.js
@@ -2,6 +2,7 @@ const info = require("debug")("app:info");
 // Input validation
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 const asyncMiddleware = require("../middleware/async");
 const authMiddleware = require("../middleware/auth");
 const admin = require("../middleware/admin");
@@ -40,6 +41,10 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
+  // needed because winston handles 500 errors and a value here that can't be cast to a valid objectId will cause winston to return 500 via middleware errors
+  if (!mongoose.Types.ObjectId.isValid(req.params.id))
+    return res.status(404).send("Invalid ID.");
+
   const genre = await Genre.findById(req.params.id);
 
   if (!genre)
