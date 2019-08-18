6d1a0c87a91854c8562acc7b49a7059cd1d4f13e
a <awatkin.work@gmail.com>
2019-08-18 23:57:09 +0100

    refactored and added notes on how to refactor tests

--- vidly-exercise/tests/postGenre.test.js
+++ vidly-exercise/tests/postGenre.test.js
@@ -3,12 +3,19 @@ const { Genre } = require("../models/genre");
 const { User } = require("../models/user");
 let server;
 
+/* how to refactor
+
+define a happy path, then in each test change one parameter that clearly aligns with the name of the test
+
+*/
+
 describe("POST /", () => {
   // starts a new server for each test
   // if you already had a server running it would cause a conflict to import server
   beforeEach(async () => {
     server = require("../index");
     token = new User().generateAuthToken();
+    name = "genre1";
   });
 
   // closses the server after each test
@@ -21,6 +28,7 @@ describe("POST /", () => {
 
   // here to allow setting it to an empty string for testing not being logged in
   let token;
+  let genre;
 
   // tests that use this still themselves need to be async this returns a promise
   const exec = async () => {
@@ -28,9 +36,7 @@ describe("POST /", () => {
       .post("/api/genres")
       .set("X-Auth-Token", token)
       .send({
-        // gives an array of 52 elements with a between
-        // the elements are actually empty items
-        name: "genre1"
+        name
       });
   };
 
@@ -43,58 +49,30 @@ describe("POST /", () => {
   });
 
   it("should return a 401 if client not logged in", async () => {
-    const res = await request(server)
-      .post("/api/genres")
-      .send({
-        name: "genre1"
-      });
-
+    token = "";
+    const res = await exec();
     expect(res.status).toBe(401);
   });
 
   it("should return a 400 if genre is less than 5 characters", async () => {
-    const token = new User().generateAuthToken();
-
-    const res = await request(server)
-      .post("/api/genres")
-      .set("X-Auth-Token", token)
-      .send({
-        name: "1234"
-      });
+    name = "1234";
+    const res = await exec();
 
     expect(res.status).toBe(400);
   });
 
   it("should return a 400 if genre is more than 50 characters", async () => {
-    const token = new User().generateAuthToken();
-
-    const res = await request(server)
-      .post("/api/genres")
-      .set("X-Auth-Token", token)
-      .send({
-        // gives an array of 52 elements with a between
-        // the elements are actually empty items
-        name: new Array(52).join("a")
-      });
-
+    // gives an array of 52 elements with a between
+    // the elements are actually empty items
+    name = new Array(52).join("a");
+    const res = await exec();
     expect(res.status).toBe(400);
   });
 
   it("should return a 200 along with the posted genre", async () => {
-    const token = new User().generateAuthToken();
-
-    const res = await request(server)
-      .post("/api/genres")
-      .set("X-Auth-Token", token)
-      .send({
-        // gives an array of 52 elements with a between
-        // the elements are actually empty items
-        name: "comedy"
-      });
-
+    const res = await exec();
     expect(res.status).toBe(200);
-
-    const genre = await Genre.find({ name: "Comedy" });
+    const genre = await Genre.find({ name });
     expect(genre).not.toBeNull();
   });
 });
