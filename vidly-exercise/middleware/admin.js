69955e94ceb7b61b091ab912c3be8dfbc5dcc115
a <awatkin.work@gmail.com>
2019-08-19 17:37:31 +0100

    some notes

--- vidly-exercise/middleware/admin.js
+++ vidly-exercise/middleware/admin.js
@@ -1,9 +1,9 @@
 module.exports = function(req, res, next) {
-  // 401 unauthorized - try again
+  // 401 unauthorized - try again, if credentials are wrong you should return this
 
   // 403 forbidden - don't try again you can't access this resource
 
-  // denies access
+  // denies access - here it's 403 because the person calling is not an admin
   if (!req.user.isAdmin) return res.status(403).send("Access denied.");
 
   // grants access
