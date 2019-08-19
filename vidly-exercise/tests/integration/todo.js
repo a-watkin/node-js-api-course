ee2a549d2210ef48c85714b5677177149e3bb6f6
a <awatkin.work@gmail.com>
2019-08-20 00:48:22 +0100

    notes for tdd routes

---
+++ vidly-exercise/tests/integration/todo.js
@@ -0,0 +1,17 @@
+// POST /api/returns {customerId, movieId}
+
+// Return 401 if client is not logged in
+
+// Return 400 if cusotmerId is not provided
+
+// Return 400 if movieId is not provided
+
+// Return 404 no rental found
+
+// Return 400 if rental already returned
+
+// Return 200 if return is ok
+// Set return date
+// Calculate fee
+// Increase stock
+// Return summary of rental
