aa816717fcfd2a42650152fad7560a16cb5db890
a <awatkin.work@gmail.com>
2019-08-19 04:00:43 +0100

    some refactoring to clean things up

--- vidly-exercise/routes/genre.js
+++ vidly-exercise/routes/genre.js
@@ -101,67 +101,47 @@ router.post("/", authMiddleware, async (req, res) => {
 router.put("/:id", authMiddleware, async (req, res) => {
   const validationResult = validateGenre(req.body);
 
-  info(validationResult);
   if (validationResult.error) {
     return res
       .status(400)
       .send(
-        "The name of a genre must be a string and must be at least 3 characters long."
+        "The name of a genre must be a string between 5 and 50 characters long."
       );
   }
 
-  try {
-    info("id is ", req.params.id);
-    const result = await Genre.findOne({ _id: req.params.id });
-    info("result is ", result.genre);
-    if (result) {
-      // update the genre
-      result.name = req.body.name;
+  const result = await Genre.findOne({ _id: req.params.id });
 
-      try {
-        const savedUpdate = await result.save();
-        return res.status(200).send(savedUpdate);
-      } catch (error) {
-        return res.status(500).send("Problem updating the genre ", error);
-      }
-    } else {
-      return res.status(404).send("Genre not found.");
-    }
-  } catch (error) {
-    return res.status(500).send("autism beavers");
+  if (!result) {
+    return res.status(404).send("Genre not found.");
   }
+
+  result.name = req.body.name;
+  const savedUpdate = await result.save();
+  return res.status(200).send(savedUpdate);
 });
 
 // this method is a bit dumb isn't it?
-router.delete("/delete", authMiddleware, async (req, res) => {
-  try {
-    const genres = await Genre.deleteMany();
-    info(genres);
+// router.delete("/delete", authMiddleware, async (req, res) => {
+//   try {
+//     const genres = await Genre.deleteMany();
+//     info(genres);
 
-    return res.status(200).send(genres);
-  } catch (error) {
-    return res.status(500).send("Error deleting genres.");
-  }
-});
+//     return res.status(200).send(genres);
+//   } catch (error) {
+//     return res.status(500).send("Error deleting genres.");
+//   }
+// });
 
 // each middleware in the array will be called in turn
 router.delete("/:id", [authMiddleware, admin], async (req, res) => {
-  try {
-    info(req.params.id);
-    const result = await Genre.findOne({ _id: req.params.id });
+  const result = await Genre.findOne({ _id: req.params.id });
 
-    if (!result) {
-      return res.status(404).send("Genre not found.");
-    }
-    try {
-      const deletedGenre = await Genre.deleteOne({ _id: req.params.id });
-      return res.status(200).send(result);
-    } catch (error) {
-      return res.status(500).send("Genre not deleted.");
-    }
-  } catch (error) {
-    res.send("something went wrong");
+  if (!result) {
+    return res.status(404).send("Genre not found.");
   }
+
+  await Genre.deleteOne({ _id: req.params.id });
+  return res.status(200).send(result);
 });
 
 module.exports = router;
